1b662044c8d06296f72780cced931f905d0b84cd
chinanugod <chinanugodwin@gmail.com>
2025-10-27 14:14:31 +0100

    Implement splash screen hide logic with JS

---
+++ scripts.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+  const splash = document.getElementById("splash-screen");
+  setTimeout(() => {
+    splash.style.display = "none";
+  }, 3000); // 3 seconds
+});
